9710dd80a7527744d4532eef7eafb8fde49c1a40
Khoa Le <khoale9098@gmail.com>
2021-01-25 23:06:39 +0700

    build layout pinned

--- components/common/catagory_container/index.tsx
+++ components/common/catagory_container/index.tsx
@@ -1,6 +1,16 @@
-const CatagoryContainer = () => {
+import { FC } from 'react'
+import classnames from 'classnames'
+import s from './catagory.module.css'
+
+const CatagoryContainer: FC<{
+  color: string | 'yellow' | 'green'
+  title: string
+}> = ({ color, title }) => {
   return (
-    <div>
+    <div className={s.pinned}>
+      <h2>
+        <span>Announcements</span>
+      </h2>
       <div>1</div>
     </div>
   )
--- container/home/Pinned/index.tsx
+++ container/home/Pinned/index.tsx
@@ -1,10 +1,15 @@
+import CatagoryContainer from '@components/common/catagory_container'
 import s from './pinned.module.css'
 
 const Pinned = () => {
   return (
     <section className="flex">
-      <div className="w-3/12 flex ">25%</div>
-      <div className="w-9/12">75%</div>
+      <div className="w-3/12 flex ">
+        <CatagoryContainer>dsd</CatagoryContainer>
+      </div>
+      <div className="w-9/12 ml-8">
+        <CatagoryContainer>dsd</CatagoryContainer>
+      </div>
     </section>
   )
 }
