4f16f72cb2e36704bc71d3830b6ffc420323addb
Khoa Le <khoale9098@gmail.com>
2021-01-31 13:28:20 +0700

    impl pinned

---
+++ components/common/pinned_container/index.tsx
@@ -0,0 +1,37 @@
+import { FC, CSSProperties } from 'react'
+import classnames from 'classnames'
+
+import s from './pinned_container.module.css'
+
+type styled = {
+  primary: string
+  secondary: string
+}
+
+const styledBorder = {
+  primary: 'border-primary',
+  secondary: 'border-secondary',
+} as styled
+
+const styledBackground = {
+  primary: 'bg-primary',
+  secondary: 'bg-secondary',
+} as styled
+
+const PinnedContainer: FC<{
+  color: 'primary' | 'secondary'
+  title: string
+  className?: string
+  style?: CSSProperties
+}> = ({ color, title, children, className = '' }) => {
+  return (
+    <div className={classnames(s.pinned, s.special, styledBorder[color], className)}>
+      <h2 className={styledBackground[color]}>
+        <span>{title}</span>
+      </h2>
+      {children}
+    </div>
+  )
+}
+
+export default PinnedContainer
--- container/home/Pinned/index.tsx
+++ container/home/Pinned/index.tsx
@@ -1,14 +1,22 @@
-import CatagoryContainer from '@components/common/catagory_container'
-import s from './pinned.module.css'
+import PinnedContainer from '@components/common/pinned_container'
+import classnames from 'classnames'
+import PinnedPost from '@components/common/pinned_post'
+import Link from 'next/link'
 
 const Pinned = () => {
   return (
     <section className="flex">
       <div className="w-3/12 flex ">
-        <CatagoryContainer>dsd</CatagoryContainer>
+        <PinnedContainer className="global-special" title="Annoucement" color="primary">
+          <PinnedPost title="Xin chào mình là Khoa?" />
+        </PinnedContainer>
       </div>
       <div className="w-9/12 ml-8">
-        <CatagoryContainer>dsd</CatagoryContainer>
+        <PinnedContainer className="global-special" title="Don't miss it" color="secondary">
+          <PinnedPost title="Xin chào mình là Khoa?" />
+          <PinnedPost title="Xin chào mình là Khoa?" />
+          <PinnedPost title="Xin chào mình là Khoa?" />
+        </PinnedContainer>
       </div>
     </section>
   )
