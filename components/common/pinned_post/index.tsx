2c32862dee606e4e0db547a2b43f526006106814
Khoa Le <khoale9098@gmail.com>
2021-02-02 23:26:19 +0700

    image blog huhu

--- components/common/pinned_post/index.tsx
+++ components/common/pinned_post/index.tsx
@@ -1,21 +1,34 @@
 import { FC } from 'react'
 import Link from 'next/link'
+import classnames from 'classnames'
+import s from './pinned_post.module.css'
 
 const PinnedPost: FC<{
   title: string
+  img?: string
   description?: string
   actor?: string
   hastag?: string
-}> = ({ title, description, actor, hastag }) => {
+}> = ({ title, description, actor, hastag, img }) => {
   return (
     <article>
-      <h3>
+      <div className={classnames(s.iimage)}>
+        <div>
+          <img src={img} alt="" />
+        </div>
+      </div>
+      <h3 className="pt-4">
         <Link href="/">
-          <a className="global-underline">Xin chào, mình là Khoa?</a>
+          <a className="global-underline">{title}</a>
         </Link>
       </h3>
-      <div className="meta">
+      <div className="text-sm relative w-full font-mono">
         <a>Khoa Le and others</a>
+      </div>
+      <div className="m-0 flex relative w-full text-xs mt-2 font-mono">
+        <a className="px-1">#creative</a>
+        <a className="px-1">#review</a>
+        <a className="px-1">#coding</a>
       </div>
     </article>
   )
--- container/home/Pinned/index.tsx
+++ container/home/Pinned/index.tsx
@@ -1,21 +1,28 @@
 import PinnedContainer from '@components/common/pinned_container'
-import classnames from 'classnames'
 import PinnedPost from '@components/common/pinned_post'
-import Link from 'next/link'
 
 const Pinned = () => {
   return (
     <section className="flex">
       <div className="w-3/12 flex ">
-        <PinnedContainer className="global-special" title="Annoucement" color="primary">
+        <PinnedContainer title="Annoucement" color="primary">
           <PinnedPost title="Xin chào mình là Khoa?" />
         </PinnedContainer>
       </div>
       <div className="w-9/12 ml-8">
-        <PinnedContainer className="global-special" title="Don't miss it" color="secondary">
-          <PinnedPost title="Xin chào mình là Khoa?" />
-          <PinnedPost title="Xin chào mình là Khoa?" />
-          <PinnedPost title="Xin chào mình là Khoa?" />
+        <PinnedContainer title="Don't miss it" color="secondary">
+          <PinnedPost
+            title="Thử Rồi Thích: 5 Bước thay đổi thói quen không cần nhiều ý chí"
+            img="https://vietcetera.com/uploads/images/02-feb-2021/20201210-travanconnong-01-2-536x356.jpg"
+          />
+          <PinnedPost
+            title="Everything has beauty, but not everyone sees it"
+            img="https://vietcetera.com/uploads/images/01-feb-2021/0201-trt-habit-feature-536x356-1612170125881.jpg"
+          />
+          <PinnedPost
+            title="Everything has beauty, but not everyone sees it"
+            img="https://vietcetera.com/uploads/images/01-feb-2021/0201-trt-habit-feature-536x356-1612170125881.jpg"
+          />
         </PinnedContainer>
       </div>
     </section>
