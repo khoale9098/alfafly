98c1595c9fbe853d472397b2d8994608700e08ec
KHOALE <khoale@KHOALEs-MacBook-Pro.local>
2021-02-06 00:00:28 +0700

    fix

--- container/home/index.tsx
+++ container/home/index.tsx
@@ -4,14 +4,8 @@ import Loop from '@container/home/Loop'
 const HomeUI = () => {
   return (
     <div>
-      <h1
-        className="font-extrabold h-full text-center flex flex-col items-center justify-center text-7xl"
-        style={{ height: '40vh' }}
-      >
-        Comming Soon
-      </h1>
-      {/* <Pinned />
-      <Loop /> */}
+      <Pinned />
+      <Loop />
     </div>
   )
 }
