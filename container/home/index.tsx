88c2a380b2b29da2fe713188a71e4846d617c7e2
Khoa Le <khoale9098@gmail.com>
2021-02-05 12:00:14 +0700

    push

--- container/home/index.tsx
+++ container/home/index.tsx
@@ -4,9 +4,11 @@ import Loop from '@container/home/Loop'
 const HomeUI = () => {
   return (
     <div>
-      <h1 className="font-bold h-full flex flex-col items-center justify-center text-7xl" style={{ height: '40vh' }}>
+      <h1
+        className="font-extrabold h-full text-center flex flex-col items-center justify-center text-7xl"
+        style={{ height: '40vh' }}
+      >
         Comming Soon
-        <br />
       </h1>
       {/* <Pinned />
       <Loop /> */}
